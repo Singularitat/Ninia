9700655e12ed2a3bbca4eb7730671abab3835607
singularitat <singularbotnet@gmail.com>
2021-07-25 20:42:29 +1200

    Update Bot.java to work with new commands

--- bot/src/main/java/Ninia/Bot.java
+++ bot/src/main/java/Ninia/Bot.java
@@ -2,147 +2,50 @@ package Ninia;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.*;
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.exceptions.PermissionException;
-import net.dv8tion.jda.api.exceptions.RateLimitedException;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.EmbedBuilder;
-
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.Properties;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.IOException;
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.RuntimeMXBean;
+import Ninia.commands.*;
+import Ninia.utils.CommandClientBuilder;
+import Ninia.utils.EventWaiter;
+
+import java.util.Properties;
 
 import javax.security.auth.login.LoginException;
 
-import java.time.OffsetDateTime;
-
-import java.awt.Color;
-
 
 public class Bot extends ListenerAdapter {
-    public static void main(String[] args) {
-        try {
-            InputStream input = new FileInputStream("src/main/java/Ninia/bot.properties");
-            Properties config = new Properties();
-            config.load(input);
+    public static void main(String[] args) throws IOException, LoginException, InterruptedException {
+        InputStream input = new FileInputStream("src/main/java/Ninia/bot.properties");
+        Properties config = new Properties();
+        config.load(input);
 
-            JDA jda = JDABuilder.createDefault(config.getProperty("token"))
-                .addEventListeners(new Bot())
-                .build();
-            jda.awaitReady();
-        } catch (LoginException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+        EventWaiter waiter = new EventWaiter();
+        CommandClientBuilder client = new CommandClientBuilder();
 
-    @Override
-    public void onMessageReceived(MessageReceivedEvent event) {
-        long currentTime = System.currentTimeMillis();
+        client.useDefaultGame();
+        client.setOwnerId(config.getProperty("owner_id"));
+        client.setEmojis("\uD83D\uDE03", "\uD83D\uDE2E", "\uD83D\uDE26");
+        client.setPrefix("!");
+        client.addCommands(
+            new CatCommand(),
+            new ChooseCommand(),
+            new PingCommand(),
+            new InviteCommand(),
+            new RollCommand(),
+            new SlotCommand(),
+            new SourceCommand(),
+            new UptimeCommand()
+        );
 
-        JDA jda = event.getJDA();
-        long responseNumber = event.getResponseNumber();
-
-        User author = event.getAuthor();
-        Message message = event.getMessage();
-        MessageChannel channel = event.getChannel();
-
-        String msg = message.getContentDisplay();
-        boolean bot = author.isBot();
-
-        if (event.isFromType(ChannelType.TEXT)) {
-            Guild guild = event.getGuild();
-            TextChannel textChannel = event.getTextChannel();
-            Member member = event.getMember();
-
-            String name;
-            if (message.isWebhookMessage()) {
-                name = author.getName();
-            } else {
-                name = member.getEffectiveName();
-            }
-
-            System.out.printf("%s/%s <%s>: %s\n", guild.getName(), textChannel.getName(), name, msg);
-
-        } else if (event.isFromType(ChannelType.PRIVATE)) {
-            PrivateChannel privateChannel = event.getPrivateChannel();
-            System.out.printf("[PRIV]<%s>: %s\n", author.getName(), msg);
-        }
-
-        switch (msg) {
-            case "!ping":
-                EmbedBuilder embed = new EmbedBuilder();
-                embed.setColor(new Color(0x7289da));
-
-                long ping = (currentTime - message.getTimeCreated().toInstant().toEpochMilli());
-
-                if (ping <= 50) {
-                    embed.addField("Command Latency", "Clock out of sync", false);
-                } else {
-                    embed.addField("Command Latency", String.format("`%dms`", ping), false);
-                }
-
-                embed.addField("Discord API Latency", String.format("`%dms`", jda.getGatewayPing()), false);
-
-                channel.sendMessage(embed.build()).queue();
-                break;
-            case "!roll":
-                Random rand = ThreadLocalRandom.current();
-                int roll = rand.nextInt(6) + 1;
-                channel.sendMessage("" + roll).queue();
-                break;
-            case "!source":
-                channel.sendMessage("https://github.com/Singularitat/Ninia").queue();
-                break;
-            case "!uptime":
-                long upTime = ManagementFactory.getRuntimeMXBean().getUptime();
-                channel.sendMessage(String.format("%ds", upTime / 1000)).queue();
-                break;
-            case "!slot":
-                String[] emojis = {
-                    ":apple:",
-                    ":tangerine:",
-                    ":pear:",
-                    ":lemon:",
-                    ":watermelon:",
-                    ":grapes:",
-                    ":strawberry:",
-                    ":cherries:",
-                    ":kiwi:",
-                    ":pineapple:",
-                    ":coconut:",
-                    ":peach:",
-                    ":mango:"
-                };
-                rand = ThreadLocalRandom.current();
-
-                String a = emojis[rand.nextInt(13)];
-                String b = emojis[rand.nextInt(13)];
-                String c = emojis[rand.nextInt(13)];
-                String d = emojis[rand.nextInt(13)];
-
-                embed = new EmbedBuilder();
-                embed.setColor(new Color(0x7289da));
-
-                embed.setTitle(String.format("[ %s %s %s %s ]", a, b, c, d), null);
-                channel.sendMessage(embed.build()).queue();
-                break;
-            case "!invite":
-                channel.sendMessage("https://discord.com/oauth2/authorize?client_id=787522276500832297&scope=bot&permissions=8589934591").queue();
-                break;
-        }
+        JDA jda = JDABuilder.createDefault(config.getProperty("token"))
+            .setActivity(Activity.playing("Playing with snakebot"))
+            .addEventListeners(waiter, client.build())
+            .build();
+        jda.awaitReady();
     }
 }
